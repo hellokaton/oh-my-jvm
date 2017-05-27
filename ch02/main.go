fb50a24a2432e1ebdf8e2b5f55aa82ff89faf8d0
biezhi <biezhi.me@gmail.com>
2017-05-27 11:46:24 +0800

    add classpath and entry

--- ch02/main.go
+++ ch02/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/biezhi/oh-my-jvm/ch02/classpath"
+	"strings"
+)
 
 func main() {
 	cmd := parseCmd()
@@ -15,6 +19,16 @@ func main() {
 }
 
 func startJVM(cmd *Cmd) {
-	fmt.Printf("classpath:%s class:%s args:%v\n",
-		cmd.cpOption, cmd.class, cmd.args)
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	fmt.Printf("classpath:%v class:%v args:%v\n",
+		cp, cmd.class, cmd.args)
+
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Printf("Could not find or load main class %s\n", cmd.class)
+		return
+	}
+
+	fmt.Printf("class data:%v\n", classData)
 }
